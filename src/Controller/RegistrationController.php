be204b23655b1754acdc3c6cdd6530a2798821fc
ADG08 <dominguesramosandre@gmail.com>
2024-12-17 12:28:46 +0100

    big refacto 2/2

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -54,7 +54,7 @@ class RegistrationController extends AbstractController
             return $security->login($user, 'form_login', 'main');
         }
 
-        return $this->render('registration/register.html.twig', [
+        return $this->render('auth/register.html.twig', [
             'registrationForm' => $form,
         ]);
     }
